aea81da770a28211a3519df8cbdc10365af94cc1
João Rodrigues De Melo Neto <j_neto_rodrigues@hotmail.com>
2023-01-20 16:49:17 -0300

    outro quicksort

--- src/main/java/org/ufpi/QuickSort.java
+++ src/main/java/org/ufpi/QuickSort.java
@@ -7,7 +7,7 @@ public class QuickSort {
 
     public static void main(String[] args) {
         Integer[] numeros = {12, 11, 13, 5, 6, 7, 1, 3};
-        System.out.println("iteracoes " + quickSort(numeros, 0, numeros.length-1));
+        System.out.println("iteracoes " + NewquickSort(numeros, 0, numeros.length-1));
     }
 
     public static int quickSort(Integer[] arr, int low, int high) {
@@ -42,4 +42,58 @@ public class QuickSort {
 
         return i + 1;
     }
+
+    public static int NewquickSort(Integer[] vetor, int inicio, int fim) {
+        if(fim > inicio) {
+            
+          //Chamada da rotina que ira dividir o vetor em 3 partes.
+          int indexPivo = dividir(vetor, inicio, fim);
+          iterCount++;
+          /* Chamada recursiva para redivisao do vetor de elementos menores
+            que o pivô. */
+            NewquickSort(vetor, inicio, indexPivo - 1);
+          /* Chamada recursiva para redivisao do vetor de elementos maiores
+            que o pivô. */
+            NewquickSort(vetor, indexPivo + 1, fim);
+        }
+        return iterCount;
+      }
+
+
+    private static int dividir(Integer[] vetor, int inicio, int fim) {
+        int pivo, pontEsq, pontDir = fim;
+        pontEsq = inicio + 1;
+        pivo = vetor[inicio];
+    
+        while(pontEsq <= pontDir) {
+          /* Vai correr o vetor ate que ultrapasse o outro ponteiro
+            ou ate que o elemento em questão seja menor que o pivô. */
+          while(pontEsq <= pontDir && vetor[pontEsq] <= pivo) {
+            pontEsq++;
+          }
+    
+          /* Vai correr o vetor ate que ultrapasse o outro ponteiro
+            que o elemento em questão seja maior que o pivô. */
+          while(pontDir >= pontEsq && vetor[pontDir] > pivo) {
+            pontDir--;
+          }
+    
+          /* Caso os ponteiros ainda nao tenham se cruzado, significa que valores
+            menores e maiores que o pivô foram localizados em ambos os lados.
+            Trocar estes elementos de lado. */
+          if(pontEsq < pontDir) {
+            trocar(vetor, pontDir, pontEsq);
+            pontEsq++;
+            pontDir--;
+          }
+        }
+    
+        trocar(vetor, inicio, pontDir);
+        return pontDir;
+      }
+      private static void trocar(Integer[] vetor, int i, int j) {
+        int temp = vetor[i];
+        vetor[i] = vetor[j];
+        vetor[j] = temp;
+      }
 }
